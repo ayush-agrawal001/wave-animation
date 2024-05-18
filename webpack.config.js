5f341929c9b05402d33dc9273ac903c1ad22fe83
ayush-agrawal001 <131985798+ayush-agrawal001@users.noreply.github.com>
2024-05-19 00:48:49 +0530

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+module.exports = {
+    entry: './index.mjs',
+    output: {
+        path: path.resolve(__dirname, 'D:/Web development/HTML_Canvas/canvas_9'),
+        filename: 'bundle.js'
+    }
+};
